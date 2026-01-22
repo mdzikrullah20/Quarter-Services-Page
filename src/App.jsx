f4fc7ab9cd7190bcbebdab634d1389c9edc126ce
Md Zikrullah <mdazharuddin155332@gmail.com>
2026-01-23 01:02:24 +0530

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./Components/About";
 import Navbaar from "./Components/Navbaar";
 import NavTop from "./Components/NavTop";
 import Footer from "./Components/Footer";
-import News from "./Components/news";
+import News from "./Components/News";
 import Contact from "./Components/Contact";
 import Property from "./Components/Property";
 function App() {
